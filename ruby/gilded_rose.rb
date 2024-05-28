8e007b480adebda720af07cbb60cab24f635589b
work <olenevo007@gmail.com>
2024-05-28 13:58:42 +0300

    test work

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,50 +6,46 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_item_quality(item)
+      update_item_sell_in(item)
     end
+  end
+
+  private
+
+  def update_item_quality(item)
+    case item.name
+    when "Aged Brie"
+      increase_quality(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      update_backstage_pass_quality(item)
+    when "Conjured Mana Cake"
+      decrease_quality(item, 2)
+    when "Sulfuras, Hand of Ragnaros"
+      return
+    else
+      decrease_quality(item)
+    end
+  end
+
+  def update_backstage_pass_quality(item)
+    increase_quality(item)
+    increase_quality(item) if item.sell_in < 11
+    increase_quality(item) if item.sell_in < 6
+    decrease_quality(item) if item.sell_in < 1
+  end
+
+  def increase_quality(item)
+    item.quality += 1 if item.quality < 50
+  end
+
+  def decrease_quality(item, value = 1)
+    item.quality -= value if item.quality > 0
+    item.quality = 0 if item.sell_in < 1
+  end
+
+  def update_item_sell_in(item)
+    item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
   end
 end
 
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,9 +3,59 @@ require 'rspec'
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "fixme"
+  context "update_quality method" do
+    it "reduces quality by 1 for normal items" do
+      items = [Item.new("Normal Item", 5, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(9)
+    end
+
+    it "reduces quality by 2 for Conjured items" do
+      items = [Item.new("Conjured Mana Cake", 5, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(8)
+    end
+
+    it "increases quality for Aged Brie" do
+      items = [Item.new("Aged Brie", 5, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(11)
+    end
+
+    it "does not increase quality above 50" do
+      items = [Item.new("Aged Brie", 5, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(50)
+    end
+
+    it "keeps quality at 80 for Sulfuras" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 80)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(80)
+    end
+
+    it "increases quality for Backstage passes" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(21)
+    end
+
+    it "increases quality by 2 when sell_in is 10 or less for Backstage passes" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(22)
+    end
+
+    it "increases quality by 3 when sell_in is 5 or less for Backstage passes" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(23)
+    end
+
+    it "sets quality to 0 after the concert date for Backstage passes" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq(0)
+    end
   end
 end
